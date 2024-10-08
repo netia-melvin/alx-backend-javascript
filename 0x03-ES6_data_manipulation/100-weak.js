78a90712eecebef16dd9d55be2ef96e159cb55de
netia-melvin <melvinatemba18@gmail.com>
2024-10-08 10:00:07 +0000

    fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,3 @@
-/**
- * A weak map of endpoints and the number of calls made.
- */
-export const weakMap = new WeakMap();
-
-/**
- * The maximum number of calls for an endpoint.
- */
-const MAX_ENDPOINT_CALLS = 5;
 
 /**
  * Tracks the number of calls made to an API's endpoint.
@@ -14,13 +5,21 @@ const MAX_ENDPOINT_CALLS = 5;
  *   protocol: String,
  *   name: String,
  * }} endpoint - The endpoint to make a request to.
- * @author Melvin Netia <https://github.com/netia-melvin>
+ * @author Melvin Netia <https://github.com/netia
  */
+
+export const weakMap = new WeakMap();
+
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
+  let called = weakMap.get(endpoint) || 0;
+
+  called += 1;
+
+  weakMap.set(endpoint, called);
+
+  if (called >= 5) {
+    throw Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
-    throw new Error('Endpoint load is high');
-  }
+
+  return called;
+}
