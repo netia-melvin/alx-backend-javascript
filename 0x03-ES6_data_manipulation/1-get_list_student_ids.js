68725f16993b6b755bdb91fb9e8adbeff6ddb801
netia-melvin <melvinatemba18@gmail.com>
2024-10-08 07:21:42 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,16 @@
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Melvin Netia <https://github.com/netia-melvin>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
